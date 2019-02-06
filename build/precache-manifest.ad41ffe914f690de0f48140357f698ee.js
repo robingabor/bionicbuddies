e366d6d50c1aeac309d929b199ee3794c71eb53e
Robin Gabor <grobin03@hotmail.com>
2019-02-06 22:57:37 +0100

    adding starting project

---
+++ build/precache-manifest.ad41ffe914f690de0f48140357f698ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be98c21ad0ec9e5fcd72",
+    "url": "/static/js/main.be98c21a.chunk.js"
+  },
+  {
+    "revision": "9eef4532a4acbf081515",
+    "url": "/static/js/1.9eef4532.chunk.js"
+  },
+  {
+    "revision": "be98c21ad0ec9e5fcd72",
+    "url": "/static/css/main.a38c6a1b.chunk.css"
+  },
+  {
+    "revision": "9eef4532a4acbf081515",
+    "url": "/static/css/1.383acf42.chunk.css"
+  },
+  {
+    "revision": "4e37ae6dd86b8ede72b25915cdce0c94",
+    "url": "/index.html"
+  }
+];
